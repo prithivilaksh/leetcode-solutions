1d681f494a7f0caf355c3052fb299679b1267293
Prithivi Lakshmanan <prithivilaksh@gmail.com>
2025-07-06 12:49:50 +0530

    added another solution for 3607

--- 3607.py
+++ 3607.py
@@ -79,6 +79,7 @@
 
 #         return res
 
+
 # class Solution:
 #     def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
 #         G = [[] for _ in range(c + 1)]
@@ -115,33 +116,75 @@
 #                 online[x] = 0
 #         return res
 
+# class Solution:
+#     def processQueries(self, c: int, conn: List[List[int]], qs: List[List[int]]) -> List[int]:
+
+#         par=[i for i in range(c+1)]
+#         on=[True] * (c+1)
+#         comppq=defaultdict(list)
+#         res=[]
+
+#         def union(a,b):
+#             a,b=find(a),find(b)
+#             par[b]=a
+        
+#         def find(x):
+#             if x==par[x]: return x
+#             par[x]=find(par[x])
+#             return par[x]
+        
+#         for u,v in conn: union(u,v)
+        
+#         for u in range(1,c+1): heappush(comppq[find(u)],u)
+        
+#         for tp,u in qs:
+#             if tp==2: on[u]=False
+#             elif on[u]: res.append(u)
+#             else:
+#                 pq=comppq[find(u)]
+#                 while pq and not on[pq[0]]: heappop(pq)
+#                 res.append(pq[0] if pq else -1)
+        
+#         return res
+
 class Solution:
     def processQueries(self, c: int, conn: List[List[int]], qs: List[List[int]]) -> List[int]:
 
-        par=[i for i in range(c+1)]
-        on=[True] * (c+1)
-        comppq=defaultdict(list)
-        res=[]
+        g=defaultdict(list)
+        comp=defaultdict(list)
+        par=[-1]*(c+1)
+        on=[True]*(c+1)
+        
+        for [u,v] in conn:
+            g[u].append(v)
+            g[v].append(u)
+        
 
-        def union(a,b):
-            a,b=find(a),find(b)
-            par[b]=a
+        def dfs(u,mi):
+            par[u]=mi
+            for v in g[u]:
+                if par[v]==-1: dfs(v,mi)
         
-        def find(x):
-            if x==par[x]: return x
-            par[x]=find(par[x])
-            return par[x]
-        
-        for u,v in conn: union(u,v)
-        
-        for u in range(1,c+1): heappush(comppq[find(u)],u)
-        
+        for u in range(1,c+1):
+            if par[u]==-1: dfs(u,u)
+            mi=par[u]
+            comp[mi].append(u)     
+
+        for vs in comp.values(): vs.reverse()       
+
+        res=[]
         for tp,u in qs:
             if tp==2: on[u]=False
             elif on[u]: res.append(u)
             else:
-                pq=comppq[find(u)]
-                while pq and not on[pq[0]]: heappop(pq)
-                res.append(pq[0] if pq else -1)
-        
+                mi=par[u]
+                while comp[mi] and not on[comp[mi][-1]]: comp[mi].pop()
+                res.append(comp[mi][-1] if comp[mi] else -1)
+
         return res
+
+        
+                
+                
+
+        
