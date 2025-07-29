e59f5127fcd298849ae38bd0d936fcca497d0344
Prithivi Lakshmanan <prithivilaksh@gmail.com>
2025-07-29 09:32:54 +0530

    added 76

---
+++ 76.py
@@ -0,0 +1,66 @@
+
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+
+#         mpt,mps=defaultdict(int),defaultdict(int)
+#         for c in t: mpt[c]+=1
+
+#         l,n,cnt=0,len(t),0
+#         ires,res=len(s)+1,""
+#         for r,_ in enumerate(s):
+#             if mpt[s[r]]>mps[s[r]]:cnt+=1
+#             mps[s[r]]+=1
+
+#             while l<r and mpt[s[l]]<mps[s[l]]:
+#                 mps[s[l]]-=1
+#                 l+=1
+
+#             if cnt==n and r-l+1<ires: 
+#                 ires=r-l+1
+#                 res=s[l:r+1]
+        
+#         return res
+
+
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+
+#         mp=defaultdict(int)
+#         for c in t: mp[c]+=1
+
+#         l,cnt=0,len(t)
+#         ires,res=len(s)+1,""
+#         for r,_ in enumerate(s):
+
+#             if mp[s[r]]>0:cnt-=1
+#             mp[s[r]]-=1
+#             while cnt==0:
+#                 if r-l+1<ires: ires,res=r-l+1,s[l:r+1]
+#                 if mp[s[l]]==0: cnt+=1
+#                 mp[s[l]]+=1;l+=1
+            
+#         return res
+
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        mp=defaultdict(int)
+        for c in t: mp[c]+=1
+
+        l,cnt=0,len(t)
+        ires,res=len(s)+1,""
+        for r,_ in enumerate(s):
+
+            if mp[s[r]]>0:cnt-=1
+            mp[s[r]]-=1
+            if cnt==0:
+                while cnt==0:
+                    if mp[s[l]]==0: cnt+=1
+                    mp[s[l]]+=1;l+=1
+                if r-l<ires: ires,res=r-l,s[l-1:r+1]
+
+            
+        return res
+
