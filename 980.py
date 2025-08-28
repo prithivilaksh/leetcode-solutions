37fa1163a8622dff4126b3ad85b5a0113c744f64
Prithivi Lakshmanan <prithivilaksh@gmail.com>
2025-08-28 09:58:00 +0530

    added 980

---
+++ 980.py
@@ -0,0 +1,85 @@
+# class Solution:
+#     def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        
+#         m,n=len(grid),len(grid[0])
+#         ui=uj=l0=0
+#         vis=set()
+#         res=[0]
+        
+#         def dfs(i,j):
+#             if grid[i][j]==2:
+#                 if len(vis)==l0+1: res[0]+=1
+#                 return
+#             vis.add((i,j))
+#             for di,dj in ((1,0),(0,1),(-1,0),(0,-1)):
+#                 vi,vj=i+di,j+dj
+#                 if 0<=vi<m and 0<=vj<n and (vi,vj) not in vis and grid[vi][vj]!=-1:
+#                     dfs(vi,vj)
+
+#             vis.remove((i,j))
+        
+
+#         for i in range(m):
+#             for j in range(n):
+#                 if grid[i][j]==0:l0+=1
+#                 elif grid[i][j]==1:ui,uj=i,j
+#         dfs(ui,uj)
+#         return res[0]
+
+# class Solution:
+#     def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        
+#         m,n=len(grid),len(grid[0])
+#         ui=uj=l0=0
+#         vis=set()
+#         res=[0]
+        
+#         def dfs(i,j):
+#             if grid[i][j]==2:
+#                 if len(vis)==l0+2: res[0]+=1
+#                 return
+#             for di,dj in ((1,0),(0,1),(-1,0),(0,-1)):
+#                 vi,vj=i+di,j+dj
+#                 if 0<=vi<m and 0<=vj<n and (vi,vj) not in vis and grid[vi][vj]!=-1:
+#                     vis.add((vi,vj))
+#                     dfs(vi,vj)
+#                     vis.remove((vi,vj))
+
+#         for i in range(m):
+#             for j in range(n):
+#                 if grid[i][j]==0:l0+=1
+#                 elif grid[i][j]==1:ui,uj=i,j
+#         vis.add((ui,uj))
+#         dfs(ui,uj)
+#         return res[0]
+
+
+
+
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        
+        m,n=len(grid),len(grid[0])
+        ui=uj=l0=0
+        res=[0]
+        
+        def dfs(i,j,cnt):
+
+            for di,dj in ((1,0),(0,1),(-1,0),(0,-1)):
+                vi,vj=i+di,j+dj
+                if 0<=vi<m and 0<=vj<n and grid[vi][vj]!=-1:
+                    if grid[vi][vj]==2:
+                        if l0==cnt: res[0]+=1
+                        continue
+                    grid[vi][vj]=-1
+                    dfs(vi,vj,cnt+1)
+                    grid[vi][vj]=0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==0:l0+=1
+                elif grid[i][j]==1:ui,uj=i,j
+
+        grid[ui][uj]=-1
+        dfs(ui,uj,0)
+        return res[0]
