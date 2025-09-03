6f77f71fd779b9c33778d842efe50a72711e291c
Prithivi Lakshmanan <prithivilaksh@gmail.com>
2025-09-03 10:13:33 +0530

    updated 322

--- 322.py
+++ 322.py
@@ -2,24 +2,24 @@
 #     def coinChange(self, coins: List[int], amt: int) -> int:
         
 #         @cache
-#         def helper(amt):
-#             res=100000
-#             for x in coins:
-#                 if amt>=x:
-#                     res=min(res,1+helper(amt-x))
-#             return 0 if amt==0 else res
-
-#         res=helper(amt)
-#         return res if res!=100000 else -1
+#         def dp(amt):
+#             if amt==0: return 0
+#             res=inf
+#             for c in coins:
+#                 if amt>=c:
+#                     res=min(res,1+dp(amt-c))
+#             return res
+        
+#         res=dp(amt)
+#         return res if res!=inf else -1
 
 class Solution:
     def coinChange(self, coins: List[int], amt: int) -> int:
-        
-        dp=[100000]*(amt+1)
-        dp[0]=0
+
+        dp=[0]+[inf]*amt
         for i in range(amt+1):
             for j in coins:
                 if i>=j:
-                    dp[i]=min(dp[i],1+dp[i-j])
-        
-        return dp[amt] if dp[amt]!=100000 else -1
+                    dp[i]=min(dp[i],dp[i-j]+1)
+
+        return dp[amt] if dp[amt]!=inf else -1
