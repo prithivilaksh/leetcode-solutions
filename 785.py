bbdf32547515773537fade80ca934b36f441be12
Prithivi Lakshmanan <prithivilaksh@gmail.com>
2025-10-12 00:05:45 +0530

    added 785

---
+++ 785.py
@@ -0,0 +1,32 @@
+# class Solution:
+#     def isBipartite(self, g: List[List[int]]) -> bool:
+        
+#         def isBP(u,c):
+#             color[u]=c
+#             for v in g[u]:
+#                 if color[v]==c: return False
+#                 elif color[v]==-1 and not isBP(v,not c): return False
+#             return True
+
+#         color=defaultdict(lambda: -1)
+#         for u in range(len(g)):
+#             if color[u]==-1 and not isBP(u,0): return False
+#         return True
+
+class Solution:
+    def isBipartite(self, g: List[List[int]]) -> bool:
+        
+        def isBP(u,c):
+            color[u]=c
+            dq=deque([u])
+            while dq:
+                u=dq.popleft()
+                for v in g[u]:
+                    if color[v]==color[u]: return False
+                    if color[v]==-1: color[v]=not color[u];dq.append(v)
+            return True
+
+        color=defaultdict(lambda: -1)
+        for u in range(len(g)):
+            if color[u]==-1 and not isBP(u,0): return False
+        return True
