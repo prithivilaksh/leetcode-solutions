11f402299dd0add5bd8e3ceb601c7c5adc07f6aa
Prithivi Lakshmanan <prithivilaksh@gmail.com>
2026-04-05 10:27:38 +0530

    330 and 710

--- 330.py
+++ 330.py
@@ -11,13 +11,70 @@
 #             cnt+=1
 #         return cnt
 
+# class Solution:
+#     def minPatches(self, nums: List[int], n: int) -> int:
+        
+#         res,nxt=0,1
+#         for x in nums:
+#             while nxt<x and nxt<=n: nxt+=nxt;res+=1
+#             nxt+=x
+#             if nxt>n: break
+#         while nxt<=n: nxt+=nxt;res+=1
+#         return res
+
+# class Solution:
+#     def minPatches(self, nums: List[int], n: int) -> int:
+
+#         res=upto=0
+#         next=1
+#         for x in nums:
+#             while next<x and next<=n:
+#                 res+=1
+#                 upto+=next
+#                 next=upto+1
+#             if next>n: return res
+#             upto+=x
+#             next=upto+1
+
+#         while next<=n:
+#             res+=1
+#             upto+=next
+#             next=upto+1
+
+#         return res
+
+# class Solution:
+#     def minPatches(self, nums: List[int], n: int) -> int:
+
+#         m=len(nums)
+#         res=upto=i=0
+#         next=1
+
+#         while next<=n:
+#             if i<m and nums[i]<=next:
+#                 upto+=nums[i]
+#                 next=upto+1
+#                 i+=1
+#             else:
+#                 upto+=next
+#                 next=upto+1
+#                 res+=1
+        
+#         return res
+
 class Solution:
     def minPatches(self, nums: List[int], n: int) -> int:
-        
-        res,nxt=0,1
-        for x in nums:
-            while nxt<x and nxt<=n: nxt+=nxt;res+=1
-            nxt+=x
-            if nxt>n: break
-        while nxt<=n: nxt+=nxt;res+=1
+
+        m=len(nums)
+        res=i=0
+        next=1
+
+        while next<=n:
+            if i<m and nums[i]<=next:
+                next+=nums[i]
+                i+=1
+            else:
+                next+=next
+                res+=1
+                
         return res
--- 710.py
+++ 710.py
@@ -35,3 +35,30 @@ class Solution:
     def pick(self) -> int:
         i = randint(0, self.m-1)
         return self.mp.get(i, i)
+
+
+# class Solution:
+
+#     # idea/observation
+#         # 1) valid numbers [0,n-1] - blacklist
+#         # 2) valid numbers count N= n-m
+#         # 3) for every blacklisted number before N create a map to a valid number after N
+
+
+#     def __init__(self, n: int, blacklist: List[int]):
+#         blacklist=set(blacklist)
+#         N=n-len(blacklist)
+#         ks=[ x for x in blacklist if x < N]
+#         vs=[ x for x in range(N,n) if x not in blacklist]
+#         self.N,self.kv=N,{k:v for k,v in zip(ks,vs)}
+
+#     def pick(self) -> int:
+#         i=random.randint(0,self.N-1)
+#         return self.kv.get(i,i)
+
+        
+
+
+# # Your Solution object will be instantiated and called as such:
+# # obj = Solution(n, blacklist)
+# # param_1 = obj.pick()
