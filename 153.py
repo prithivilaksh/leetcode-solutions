95a308ae01d56488b58b26ecf1afd472e5eba287
Prithivi Lakshmanan <prithivilaksh@gmail.com>
2026-07-19 15:05:25 +0530

    Update 153.py

--- 153.py
+++ 153.py
@@ -4,7 +4,58 @@ class Solution:
         l,r=0,len(nums)-1
         while l<r:
             m=l+(r-l)//2
-            # if nums[l]<nums[r]: return nums[l]2
+            # if nums[l]<nums[r]: return nums[l]
             if nums[m]>nums[r]: l=m+1
             else: r=m
         return nums[r]
+
+
+
+# class Solution:
+#     def findMin(self, nums: List[int]) -> int:
+        
+#         #  /
+#         # /
+#         #          /
+#         #         /
+
+#         l,r=0,len(nums)-1
+
+#         while l<r:
+#             m=l+(r-l)//2
+#             if nums[l]>nums[m]: r=m
+#             else: #nums[l]<nums[m]
+#                 if nums[r]<nums[l] : l=m+1
+#                 else: r=m
+        
+#         return nums[l]
+
+# class Solution:
+#     def findMin(self, nums: List[int]) -> int:
+        
+#         #  /
+#         # /
+#         #          /
+#         #         /
+
+#         l,r=0,len(nums)-1
+
+#         while l<r:
+#             m=l+(r-l)//2
+#             if nums[l]<nums[r]: break
+#             if nums[l]>nums[m]: r=m
+#             else: l=m+1
+        
+#         return nums[l]
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        l,r=0,len(nums)-1
+
+        while l<r:
+            m=l+(r-l)//2
+            if nums[m]>nums[r]: l=m+1
+            else: r=m
+        
+        return nums[l]
