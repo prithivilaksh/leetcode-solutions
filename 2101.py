a9830fcc9ee2ed2adfd1d05fc99cc07a9ed81303
Prithivi Lakshmanan <prithivilaksh@gmail.com>
2026-09-19 08:41:47 +0530

    Update 2101.py

--- 2101.py
+++ 2101.py
@@ -26,6 +26,36 @@
 #         return res
 
 
+# class Solution:
+#     def maximumDetonation(self, bombs: list[list[int]]) -> int:
+        
+#         def can_detonate(a,b):
+#             dis=(a[0]-b[0])**2+(a[1]-b[1])**2
+#             return dis<=a[2]**2
+        
+#         n=len(bombs)
+#         g,indeg=defaultdict(list),defaultdict(int)
+#         for i in range(n-1):
+#             for j in range(i+1,n):
+#                 if can_detonate(bombs[i],bombs[j]): g[i].append(j)
+#                 if can_detonate(bombs[j],bombs[i]): g[j].append(i)
+        
+#         def dfs(u):
+#             vis.add(u)
+#             for v in g[u]:
+#                 if v not in vis: dfs(v)
+        
+#         res,gvis=0,set()
+#         for i in sorted(range(n),key=lambda x:len(g[x])):
+#             if i in gvis: continue
+#             vis=set()
+#             dfs(i)
+#             res=max(res,len(vis))
+#             gvis.update(vis)
+        
+#         return res
+
+
 class Solution:
     def maximumDetonation(self, bombs: list[list[int]]) -> int:
         
@@ -46,7 +76,8 @@ class Solution:
                 if v not in vis: dfs(v)
         
         res,gvis=0,set()
-        for i in sorted(range(n),key=lambda x:len(g[x])):
+        # for i in sorted(range(n),key=lambda x:len(g[x]),reverse=True):
+        for i in range(n):
             if i in gvis: continue
             vis=set()
             dfs(i)
